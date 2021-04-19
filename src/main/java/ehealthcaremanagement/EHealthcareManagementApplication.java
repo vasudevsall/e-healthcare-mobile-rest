c68af62d3ebe96745239254f9469974461b06312
Vasu Dev Sall <vasudevsall@gmail.com>
2021-04-19 23:02:34 +0530

    intial commit

---
+++ src/main/java/ehealthcaremanagement/EHealthcareManagementApplication.java
@@ -0,0 +1,13 @@
+package ehealthcaremanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EHealthcareManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EHealthcareManagementApplication.class, args);
+	}
+
+}
