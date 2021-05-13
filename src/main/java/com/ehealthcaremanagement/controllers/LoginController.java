1717c0b4dae4bc39845c0c0d3fbcf10a90cce2b4
Vasu Dev Sall <vasudevsall@gmail.com>
2021-05-13 17:28:54 +0530

    Testing

--- src/main/java/com/ehealthcaremanagement/controllers/LoginController.java
+++ src/main/java/com/ehealthcaremanagement/controllers/LoginController.java
@@ -3,6 +3,8 @@ package com.ehealthcaremanagement.controllers;
 import com.ehealthcaremanagement.models.repository.UserModel;
 import com.ehealthcaremanagement.repositories.UserRepository;
 import com.ehealthcaremanagement.utilities.LoginUtil;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
@@ -13,15 +15,18 @@ import org.springframework.web.server.ResponseStatusException;
 
 import javax.servlet.http.HttpServletResponse;
 import java.security.Principal;
+import java.time.LocalDateTime;
 
 @Controller
 public class LoginController {
 
     @Autowired
     private UserRepository userRepository;
+    private final Logger logger = LoggerFactory.getLogger(LoginController.class);
 
     @RequestMapping(value = "/login-verify", method = RequestMethod.GET)
     public @ResponseBody UserModel verifyLogin(Principal principal, HttpServletResponse httpServletResponse) {
+        logger.info(LocalDateTime.now().toString());
         try {
             return LoginUtil.loginVerify(principal.getName(), userRepository);
         } catch (Exception e) {
